d275b7d2eb31fc6adcfeb921f2413020ef82914a
Adhitya Dharmawan Santosa <adhityadharmawan@gmail.com>
2022-10-12 21:45:02 +0700

    First Selenium

---
+++ src/main/java/com/juaracoding/selenium/ChromeDriverFirst.java
@@ -0,0 +1,41 @@
+package com.juaracoding.selenium;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ChromeDriverFirst {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "D:\\Bootcamp\\ChromeDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.tokopedia.com");
+        //memperbesar tampilan
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        //mencari element untuk input, lalu mengisi data
+        WebElement test = driver.findElement(By.xpath("//input[@class='css-1wc186l e110g5pc0']"));
+        test.sendKeys("sepatu");
+        String value = test.getAttribute("value");
+        if(value.isEmpty()){
+            //mencari element untuk button lalu tekan tombol button
+            System.out.println("belum diisi");
+            driver.close();
+        } else {
+            System.out.println("Sudah diisi");
+            driver.findElement(By.xpath("//button[@class='css-1czin5k e1v32nag1']")).click();
+            js.executeScript("window.scrollBy(0, 10000)");
+            Thread.sleep(3000);
+            driver.close();
+        }
+        //mencari element untuk button, lalu tekan tombol button
+//        driver.findElement(By.xpath("//button[@class='css-1czin5k e1v32nag1']")).click();
+
+//        Thread.sleep(5000);
+//        //untuk close windows
+//        driver.close();
+    }
+}
---
+++ src/main/java/com/juaracoding/selenium/Main.java
@@ -0,0 +1,49 @@
+package com.juaracoding.selenium;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        //system property untuk chrome driver
+//        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "D:\\Bootcamp\\ChromeDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //instatiate a ChromeDriver class
+        driver.manage().window().maximize();
+
+        String url = "https://demoqa.com/text-box";
+
+        driver.get(url);
+
+        String titleName = driver.getTitle();
+        System.out.println(titleName);
+
+        WebElement userName = driver.findElement(By.xpath("//*[@id='userName']"));
+        userName.sendKeys("Juara Coding");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+//        driver.findElement(By.id("userName")).sendKeys("JuaraCoding");
+        System.out.println("Input Username");
+        driver.findElement(By.id("userEmail")).sendKeys("Info@juaracoding.com");
+        System.out.println("Input Email");
+        driver.findElement(By.id("currentAddress")).sendKeys("Jakarta");
+        System.out.println("Input Current Address");
+        driver.findElement(By.id("permanentAddress")).sendKeys("Bekasi");
+        System.out.println("Input Permanent Adrress");
+
+        driver.findElement(By.id("submit")).click();
+        System.out.println("Button Submit Clicked");
+
+        //scrol vertikal 5000px
+        js.executeScript("window.scrollBy(0,500)");
+        System.out.println("Scroll Commited");
+
+        //delay harus pakai exception
+        Thread.sleep(15000);
+
+
+        driver.quit();
+    }
+}
